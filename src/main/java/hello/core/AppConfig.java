b0fdaef69c871b90e5873bba09fe02eb9eeb06d3
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-01-23 05:14:44 +0900

    feat: AppConfig -> SpringContainer

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,41 +8,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
-    // 설계에 관해 역할들이 구성 정보에 그대로 드러나야함.
-    // 역할과 구현이 한 눈에 들어와야 함.
-
-    /*
-     * new MemberRepository() -> memberRepository() 로 변경
-     * 애플리케이션의 전체 구성에 관해서 빠르게 파악이 가능하다는 장점이 존재함.
-     * */
-
-    // MemberService 역할
+    @Bean
     public MemberService memberService() {
-        // 구현에서는 Imple을 쓸거야!
         return new MemberServiceImpl(memberRepository());
     }
 
-    // MemberRepository 역할
-    private static MemberRepository memberRepository() {
-        // MemoryMemberRepository를 쓸거야!
+    @Bean
+    public static MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    // OrderService 역할
+    @Bean
     public OrderService orderService() {
-        // OrderServiceImpl을 쓸거야!
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    // DiscountPolicy 역할
+    @Bean
     public DiscountPolicy discountPolicy() {
-        // FixDiscountPolicy를 쓸거야!
-        // return new FixDiscountPolicy();
-
-        // RateDiscountPolicy를 쓰는걸로 바꾸자!!
         return new RateDiscountPolicy();
     }
 }
