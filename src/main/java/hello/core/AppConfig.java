b77b0087d9285ef758b35a8b532ceb831ef775dd
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-01-23 04:29:49 +0900

    feat: AppConfig Refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
-import hello.core.discount.RateDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,11 +11,35 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    // 설계에 관해 역할들이 구성 정보에 그대로 드러나야함.
+    // 역할과 구현이 한 눈에 들어와야 함.
+
+    /*
+     * new MemberRepository() -> memberRepository() 로 변경
+     * 애플리케이션의 전체 구성에 관해서 빠르게 파악이 가능하다는 장점이 존재함.
+     * */
+
+    // MemberService 역할
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        // 구현에서는 Imple을 쓸거야!
+        return new MemberServiceImpl(memberRepository());
     }
 
+    // MemberRepository 역할
+    private static MemberRepository memberRepository() {
+        // MemoryMemberRepository를 쓸거야!
+        return new MemoryMemberRepository();
+    }
+
+    // OrderService 역할
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        // OrderServiceImpl을 쓸거야!
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    // DiscountPolicy 역할
+    public DiscountPolicy discountPolicy() {
+        // FixDiscountPolicy를 쓸거야!
+        return new FixDiscountPolicy();
     }
 }
