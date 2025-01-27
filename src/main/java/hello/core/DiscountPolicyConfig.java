51a835e38f3a23e1ff8edd22fe1cec817fecaebf
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-01-27 23:56:51 +0900

    feat: Dependency Injection

---
+++ src/main/java/hello/core/DiscountPolicyConfig.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DiscountPolicyConfig {
+
+    @Bean
+    @Primary
+    public DiscountPolicy rateDiscountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+    @Bean
+    public DiscountPolicy fixDiscountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
